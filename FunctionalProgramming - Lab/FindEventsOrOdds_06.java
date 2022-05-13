5b38c623733372aca24882dc0f8e6f72f0179974
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-13 20:32:19 +0300

    Functional Programming - Lab

---
+++ FunctionalProgramming - Lab/FindEventsOrOdds_06.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FindEventsOrOdds_06 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] range = Arrays.stream(in.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        String condition = in.nextLine();
+
+
+        printNumbersIn(range[0], range[1], condition.equals("even")
+                ? n -> n % 2 == 0
+                : n -> n % 2 != 0);
+
+    }
+
+    private static void printNumbersIn(int startInclusive, int endInclusive, Predicate<Integer> predicate) {
+        System.out.println(IntStream.rangeClosed(startInclusive, endInclusive)
+                .boxed()
+                .filter(predicate)
+                .map(String::valueOf)
+                .collect(Collectors.joining(" ")));
+    }
+}
