38eade4a6ea768d700febe4a281b9cc6bb4b589a
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-17 22:18:19 +0300

    Defining Classes - Lab

---
+++ DefiningClasses - Lab/BankAccount.java
@@ -0,0 +1,25 @@
+public class BankAccount {
+    private static  int accountsCount = 1;
+    private static  double interestRate = 0.02;
+
+    private int id;
+    private double balance;
+    public BankAccount(){
+        this.id = accountsCount;
+        accountsCount++;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+    public double getInterestRate(int years) {
+        return interestRate * years * balance;
+    }
+    public void deposit (double amount) {
+        this.balance += amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
