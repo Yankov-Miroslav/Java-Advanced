34c24f54fa8473fc844381cdbc125dbeb37d3cb4
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-19 12:32:14 +0300

    Generics - Lab

---
+++ Generics - Lab/Scale/Main.java
@@ -0,0 +1,7 @@
+package Scale;
+
+public class Main {
+    public static void main(String[] args) {
+        Scale<Integer> scale = new Scale<Integer>(13, 42);
+    }
+}
---
+++ Generics - Lab/Scale/Scale.java
@@ -0,0 +1,20 @@
+package Scale;
+
+public class Scale<T extends Comparable<T>> {
+    public final T left;
+    public final T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier() {
+        int compared = left.compareTo(right);
+        if (compared > 0) {
+            return left;
+        } else if (compared < 0) {
+            return right;
+        } else return null;
+    }
+}
