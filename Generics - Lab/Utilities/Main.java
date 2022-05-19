47cb23fe18f369d8089f783cfee5fd09f6437023
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-19 12:32:39 +0300

    Generics - Lab

---
+++ Generics - Lab/Utilities/Main.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(13, 42, 73, 69);
+        List<String> strings = List.of("13", "42", "73", "69");
+
+        System.out.println(ListUtils.getMin(integers));
+        System.out.println(ListUtils.getMax(strings));
+    }
+}
