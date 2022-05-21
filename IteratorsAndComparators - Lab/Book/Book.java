9a632d3a9d86a52b320fab1365f35b64fd0779c9
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-21 20:52:46 +0300

    Iterators And Comparators - Lab

---
+++ IteratorsAndComparators - Lab/Book/Book.java
@@ -0,0 +1,46 @@
+package Book;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.setAuthors(authors);
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.stream(authors).collect(Collectors.toList());
+    }
+
+
+
+}
