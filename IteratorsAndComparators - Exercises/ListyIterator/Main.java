fc953d1c08052c96777271b684253d69be508195
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-21 20:50:23 +0300

    Iterators And Comparators - Exercises

---
+++ IteratorsAndComparators - Exercises/ListyIterator/Main.java
@@ -0,0 +1,43 @@
+package ListyIterator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        ListyIterator listyIterator = null;
+
+        while (!line.equals("END")) {
+            String[] commandParts = line.split("\\s+");
+            switch (commandParts[0]) {
+                case "Create":
+                    if (commandParts.length > 1) {
+                        listyIterator = new ListyIterator(Arrays.copyOfRange(commandParts, 1, commandParts.length));
+                    } else {
+                        listyIterator = new ListyIterator();
+                    }
+                    break;
+                case "Move":
+                    System.out.println(listyIterator.Move());
+                    break;
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+                case "Print":
+                    try {
+                        listyIterator.print();
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "PrintAll":
+                    listyIterator.printAll();
+                    break;
+            }
+            line = scanner.nextLine();
+        }
+
+    }
+}
